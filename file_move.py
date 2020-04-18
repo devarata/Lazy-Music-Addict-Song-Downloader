6350c54d4072bef9dc3ce7f90736b6450fcfd580
Ride0401 <devarata.oza@gmail.com>
2020-04-18 17:59:58 +0530

    code to move file to MyDownloadedSongs

---
+++ file_move.py
@@ -0,0 +1,25 @@
+import os
+import shutil
+import glob
+
+def moving():
+    list_of_files = glob.glob('C:\\Users\\devar\\Downloads\\*')
+    latest_file = max(list_of_files, key=os.path.getctime)
+    latest_file = latest_file.replace('\\','\\\\')
+    filename = latest_file.strip().split("\\")
+    filename = filename[len(filename)-1]
+    print(filename)
+
+    destination="C:\\Users\\devar\\Desktop\\MyDownloadedSongs"
+
+    if not os.path.exists(destination):
+        os.makedirs(destination)
+
+    destination=destination+"\\"+filename
+    print(destination)
+
+
+    os.rename(latest_file,destination)
+
+
+moving()
