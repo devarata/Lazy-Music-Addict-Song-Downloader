e554f1810675e9d19a8e627b2c63559ac4e19b79
Ride0401 <devarata.oza@gmail.com>
2020-04-19 13:33:09 +0530

    Solved the empty string issue

--- song_downloader.py
+++ song_downloader.py
@@ -13,36 +13,38 @@ from song_input import song_name_func
 #song name to download
 
 song = song_name_func()
-song+="\n"
+
 
 
 #driver
-driver = wbd.Chrome('C:\\Users\\devar\\.wdm\\drivers\\chromedriver\\80.0.3987.106\\win32\\chromedriver.exe')
-url = "https://www.google.com/"
-driver.get(url)
+if song!="":
+    driver = wbd.Chrome('C:\\Users\\devar\\.wdm\\drivers\\chromedriver\\80.0.3987.106\\win32\\chromedriver.exe')
+    url = "https://www.google.com/"
+    driver.get(url)
+    song+="\n"
+    #search input of google
+    driver.find_element_by_xpath('//*[@id="tsf"]/div[2]/div[1]/div[1]/div/div[2]/input').send_keys(song)
+    page = Soup(driver.page_source,'lxml')
+    html= driver.page_source
+    song_link = ""
+    for link in Soup(html, features = 'lxml',parse_only=SoupStrainer('a')):
+        if link.has_attr('href'):
+            if link['href'].find("https://www.youtube.com/watch?v=")!=-1:
+                song_link = str(link['href'])
+                break
 
-#search input of google
-driver.find_element_by_xpath('//*[@id="tsf"]/div[2]/div[1]/div[1]/div/div[2]/input').send_keys(song)
-page = Soup(driver.page_source,'lxml')
-html= driver.page_source
-song_link = ""
-for link in Soup(html, features = 'lxml',parse_only=SoupStrainer('a')):
-    if link.has_attr('href'):
-        if link['href'].find("https://www.youtube.com/watch?v=")!=-1:
-            song_link = str(link['href'])
-            break
+    #url for site that converts youtube videos to mp3
+    downloader_url = "https://ytmp3.cc/en13/"
 
-#url for site that converts youtube videos to mp3
-downloader_url = "https://ytmp3.cc/en13/"
+    #downloading the song
+    driver.get(downloader_url)
+    driver.find_element_by_xpath('//*[@id="input"]').send_keys(song_link)
+    time.sleep(1)
+    driver.find_element_by_xpath('//*[@id="submit"]').click()
+    time.sleep(3)
+    driver.find_element_by_xpath('//*[@id="buttons"]/a[1]').click()
 
-#downloading the song
-driver.get(downloader_url)
-driver.find_element_by_xpath('//*[@id="input"]').send_keys(song_link)
-time.sleep(1)
-driver.find_element_by_xpath('//*[@id="submit"]').click()
-time.sleep(3)
-driver.find_element_by_xpath('//*[@id="buttons"]/a[1]').click()
-
-#file is downloading -- wait for 2mins then move the filename
-time.sleep(120)
-file_move.moving()
+    #file is downloading -- wait for 2mins then move the filename
+    time.sleep(120)
+    file_move.moving()
+    
